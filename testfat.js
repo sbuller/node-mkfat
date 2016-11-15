b8ef94c229c47992933f0f56f617570bf26bdeb0
Stefan Buller <stefan_buller@nova3.ca>
2016-11-15 13:35:09 -0600

    How to provide for entries

--- testfat.js
+++ testfat.js
@@ -10,13 +10,17 @@ let c32stat = fs.statSync(c32)
 let sysstat = fs.statSync(sys)
 let bazstat = fs.statSync(baz)
 
+let c32stream = fs.createReadStream(null, {fd: c32})
+let sysstream = fs.createReadStream(null, {fd: sys})
+let bazstream = fs.createReadStream(null, {fd: baz})
+
 let fat = new FAT
 
 fat.entry({name:'foo', type:'directory'})
 fat.entry({name:'xyzzy', type:'directory'})
-fat.entry({name:'ldlinux.c32', size:c32stat.size}, )
-fat.entry({name:'ldlinux.sys', size:sysstat.size}, )
-fat.entry({name:'baz', size:bazstat.size}, )
+c32stream.pipe(fat.entry({name:'ldlinux.c32', size:c32stat.size}))
+sysstream.pipe(fat.entry({name:'ldlinux.sys', size:sysstat.size}))
+bazstream.pipe(fat.entry({name:'baz', size:bazstat.size}))
 
 fat.entry({name:'foo/bar', type:'link'}, '/baz')
 fat.entry({name:'xyzzy/BAR.bar', type:'link'}, '/foo/bar')
